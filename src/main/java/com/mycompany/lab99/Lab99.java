226127690d00f84e543d2ca2c5458e88e683b81b
bodaalaa19 <abdelrahmanalaa19@gmail.com>
2024-12-02 15:25:35 +0200

    start project

---
+++ src/main/java/com/mycompany/lab99/Lab99.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab99;
+
+/**
+ *
+ * @author Victus
+ */
+public class Lab99 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
