bfc3276799c3c16139db4bd71579ae1645699a85
Shams-Saied <shamsssoliman@gmail.com>
2024-12-03 12:53:40 +0200

    moved to the same package

--- src/main/java/com/mycompany/lab99/ProfilePage.java
+++ src/main/java/com/mycompany/lab99/ProfilePage.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package frontend;
+package com.mycompany.lab99;
 
 import com.mycompany.lab99.User;
 import java.awt.Image;
